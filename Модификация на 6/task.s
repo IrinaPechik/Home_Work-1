aad34ab4f8e84833941fe1aaf83ebcff64edd69d
IrinaPechik <100044301+IrinaPechik@users.noreply.github.com>
2022-10-23 20:17:57 +0300

    Update task.s

--- Модификация на 6/task.s
+++ Модификация на 6/task.s
@@ -9,7 +9,6 @@
 	.globl	Task
 	.type	Task, @function
 Task:
-	endbr64
 	push	rbp
 	mov	rbp, rsp
 	mov	QWORD PTR -24[rbp], rdi				# запись rdi в arr на позиции -24
@@ -64,22 +63,3 @@ Task:
 	mov	eax, DWORD PTR -8[rbp]				# записываем в eax j
 	pop	rbp
 	ret										# выход
-	.size	Task, .-Task
-	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	1f - 0f
-	.long	4f - 1f
-	.long	5
-0:
-	.string	"GNU"
-1:
-	.align 8
-	.long	0xc0000002
-	.long	3f - 2f
-2:
-	.long	0x3
-3:
-	.align 8
-4:
